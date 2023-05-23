29f945aabf92f84b34a30a6cb6a837dcd3ccb557
HaigEmirzian <hemirzia@stevens.edu>
2023-05-23 01:33:44 -0400

    simplified App.js

--- marketplace/src/App.js
+++ marketplace/src/App.js
@@ -1,34 +1,17 @@
 import "./App.css";
 import React from "react";
 import {
-    Home,
     Logo,
+    NavBar,
     Hamburger,
-    Houses,
-    Exchange,
-    Guide,
 } from "./components/index.js";
 import "./Hamburger.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <Logo />
-            <Router>
-                <div>
-                    <Link to="/houses">Houses</Link>
-                    <Link to="/exchange">Exchange</Link>
-                    <Link to="/guide">Guide</Link>
-                </div>
-
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/houses" element={<Houses />} />
-                    <Route path="/exchange" element={<Exchange />} />
-                    <Route path="/guide" element={<Guide />} />
-                </Routes>
-            </Router>
+            <NavBar />
             <Hamburger />
         </div>
     );
--- marketplace/src/components/NavBar.js
+++ marketplace/src/components/NavBar.js
@@ -1,13 +1,28 @@
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./home";
+import Houses from "./houses";
+import Exchange from "./exchange";
+import Guide from "./guide";
 
 function NavBar() {
-  return (
-    <div>
-      <Link to="/houses">Houses</Link>
-      <Link to="/exchange">Exchange</Link>
-      <Link to="/guide">Guide</Link>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <div>
+                    <Link to="/houses">Houses</Link>
+                    <Link to="/exchange">Exchange</Link>
+                    <Link to="/guide">Guide</Link>
+                </div>
+
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/houses" element={<Houses />} />
+                    <Route path="/exchange" element={<Exchange />} />
+                    <Route path="/guide" element={<Guide />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default NavBar;
