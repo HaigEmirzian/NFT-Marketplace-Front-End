df34ac3f6d7f466354fca44cf73af84fc64af869
HaigEmirzian <hemirzia@stevens.edu>
2023-05-23 23:32:14 -0400

    cleaning up and organizing js files into folders

--- marketplace/src/App.js
+++ marketplace/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import {
     Logo,
+    Home,
     NavBar,
     Hamburger,
     Houses,
@@ -21,13 +22,14 @@ function App() {
             <NavBar />
             <Hamburger />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/houses" element={<Houses />} />
                 <Route path="/exchange" element={<Exchange />} />
                 <Route path="/guide" element={<Guide />} />
                 <Route path="/modules" element={<Modules />} />
                 <Route path="/mission" element={<Mission />} />
                 <Route path="/about" element={<About />} />
-            </Routes>  
+            </Routes>
         </div>
     );
 }
